b7f522f222d508e0b80a733a25b44868abc42fa4
Yusuf69Hendricks <yusuf69hendricks@gmail.com>
2020-08-19 12:52:13 +0200

    added final changes to frontend and added readmore button

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function (){
+    $('.menu').on('click', function (){
         $(this).toggleClass('open');
         $( '.top-nav').toggleClass('open');
     });
 
       $('.top-nav .nav-link').on('click', function (){
-        $('.menu-toggler').removeClass('open');
+        $('.menu').removeClass('open');
         $( '.top-nav').removeClass('open');
     });
 
@@ -27,3 +27,55 @@ $(document).ready(function () {
         once: true
     });
 });
+
+
+function BtnFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readmore");
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function BtnFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("readmore2");
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function BtnFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("readmore3");
+
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
